4bb22f4525d1df84411d3c0bd3197db52561ad66
Sergio Najera <sergio.najera@target.com>
2020-12-18 18:53:58 -0600

    Remapped the santa pic to a canvas

--- src/components/SantaFrame.tsx
+++ src/components/SantaFrame.tsx
@@ -1,60 +1,74 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import {FrameImage} from "../assets/santaFrames";
 
 interface Props {
     frame: FrameImage
     // in the future, upload many photos to be able to toggle
-    uploadedImage?: any
-}
-
-const styles = {
-    parent: {
-        position: 'relative',
-        top: 0,
-        left: 0,
-    },
-    image1: {
-        position: 'relative',
-        top: 0,
-        left: 0,
-        border: '1px red solid',
-    },
-    image2: {
-        position: 'absolute',
-        top: '30px',
-        left: '30px',
-        border: '1px green solid',
-    }
+    uploadedImage?: HTMLImageElement
 }
 
 /**
  * This is just the
  */
 export default ({frame, uploadedImage}: Props) => {
-    const {parent, image1, image2} = styles
     console.log(frame)
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        // @ts-ignore
+        const context = canvas.getContext('2d')
+        // convert the image to canvas
+        const image = new Image();
+        image.src = frame.image.src
+        image.onload = () => {
+            context.drawImage(
+                image,
+                0,
+                0,
+                window.innerWidth,
+                window.innerHeight,
+                // 0,
+                // 0,
+                // 300,
+                // 100
+            );
+            // ctx.getImageData on that context
+            // somewhere ImageData is used....
+            // loop through all pixels of the image
+            // if the alpha value is < 255, this means there's some transparency
+        };
+    }, [])
+
     return (
         <>
             <div style={{position: 'relative', top: 0, left: 0}}>
-                <img src={frame.image.src} style={{
-                    position: 'relative',
-                    top: 0,
-                    left: 0,
-                    height: '500px',
-                    width: '500px',
-                    zIndex: 10
-                }}/>
+                <canvas ref={canvasRef}
+                        style={{
+                            position: 'relative',
+                            top: 0,
+                            left: 0,
+                            // width: '1000px',
+                            // height: '1000px',
+                            zIndex: 10
+                        }}
+                        width={window.innerWidth}
+                        height={window.innerHeight}
+                />
                 {uploadedImage &&
                 <img
+                    width={window.innerWidth}
+                    height={window.innerHeight}
                     style={{
                         position: 'absolute',
                         top: '0px',
                         left: '0px',
-                        height: '500px',
-                        width: '500px',
                         zIndex: 1
                     }}
-                    src={uploadedImage}/>}
+                    // @ts-ignore
+                    src={uploadedImage} alt='uploadedImage'
+                />
+                }
             </div>
         </>
     )
